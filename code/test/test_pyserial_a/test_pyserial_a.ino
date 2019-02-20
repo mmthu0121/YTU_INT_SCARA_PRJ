8e63fc77103446d4d461ba121ea18000dfeadd60
Myat Min Thu <mmthu.0121@gmail.com>
2019-02-20 23:47:55 +0630

    Update test_pyserial_a.ino

--- code/test/test_pyserial_a/test_pyserial_a.ino
+++ code/test/test_pyserial_a/test_pyserial_a.ino
@@ -6,7 +6,7 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0) {
-      state = Serial.read()- '0'; //http://forum.arduino.cc/index.php?topic=288234.0
+      state = Serial.read()- '0'; //http://forum.arduino.cc/index.php?topic=288234.0 or http://forum.arduino.cc/index.php?topic=344013.0
      
       if  (state != 0){
       Serial.println(state);
